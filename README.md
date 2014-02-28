f319ab76785d3ccd5e71601c25cc15791d9db35f
Rodrigo Chappa <rodrigo.chappa@gmail.com>
2014-02-27 23:21:58 -0300

    Modificacion a cuatros.py que no optimiza tanto la generacion de expresiones Escencialmente es el mismo codigo que cuatros.py

--- README.md
+++ README.md
@@ -7,6 +7,14 @@ Se presentan 2 soluciones. Una de fuerza de bruta (cuatros.py)
 y otra que construye la solucion tomando soluciones mas pequeñas (cuatros2.py).
 (Como formar nros usando 3 nros 4, 2 nros 4, 1 nro 4, etc. )
 
+La segunda solucion presenta 2 variantes, una de ellas (cuatros2.py) 
+hace un esfuerzo por reducir los parentesis superfluos.
+
+En la segunda variante (cuatros2b.py) menos esfuerzo se hace por reducir el 
+tamaño de las expresiones, por lo que se obtienen expresiones un poco mas 
+largas en algunos casos. Escencialmente es el mismo algoritmo que
+el que se demuestra en cuatros2.py..
+
 - rodrigo.chappa@gmail.com
 - http://goo.gl/SAaM48
 
---
+++ cuatros2b.py
@@ -0,0 +1,95 @@
+# Solucion usando memoization v.2
+# Produce expresiones un poco mas largas (comparada con la version 1)
+#
+# Construye soluciones usando 1 nro 4, luego 2 nros 4, luego 3 y finalmente 4,
+# al estilo usado por Dynamic Programming
+# (Ver funcion populate_level, la que se invoca sucesivamente de nivel 1 a 4)
+#
+# Esta solucion generica permitiria usar el mismo codigo para formar soluciones
+# de 5 usos del nro 4, 6 usos, etc.
+#
+# rodrigo.chappa@gmail.com
+# http://goo.gl/SAaM48
+
+import operator
+ 
+from math import sqrt
+
+# Construccion bottom up de nums, partiendo por el diccionario de tamano 1
+# (ver populate_level() mas abajo tb)
+nums  = { 1: { 2: "r(4)", 4: "4", 24: "4!", 4.0/9.0: ".4'", .4: ".4", sqrt(4.0/9.0):"r(.4')" },
+          2: { 44: "44" }, 3: {}, 4: {} }
+
+# paren(x): escribe parentesis alrededor de la expresion x, intenta no hacerlo cuando es superfluo
+#
+# No hace el mejor esfuerzo, eso si, pero eso requiere mas logica
+def paren(x,level):
+    return "(" + x + ")" if level > 1 else x
+
+# acerca al entero mas cercano, cuando esta cerca de este
+ferror = 0.00001
+def roundme(x):
+    if abs(x- int(x+ferror)) < ferror: return int(x+ferror)
+    return x
+
+# construye tabla de memoization usando los operadores que se pasan
+# en parametro funcs, la tabla se usa en invocaciones posteriores
+def populate_level(funcs,level):
+
+   ll = {}  # el largo de cada expresion en este nivel, la idea es usar esto para generar expresiones cortas
+   nl = nums[level]
+   for i in range(1,level):
+       # generar todas las combinaciones posibles
+       # usar esas combinaciones para operarlas con todos los operadores
+       ni  = nums[i]
+       nli = nums[level-i]
+       newelements = [ (roundme(funcs[op](x,y)), paren(ni[x],i) + op + paren(nli[y],level-i)) for op in funcs for x in ni for y in nli ]
+       # se tienen varios resultados, tomemos lo mejor
+       for k,v in newelements:
+           # se usa una expresion mas corta cada vez que se puede
+           lv = len(v)
+           if k not in nl or lv < ll[k]:
+              nl[k] = v
+              ll[k] = lv
+
+## Para comprobar que los datos generados son correctos
+##def evalme(expr):
+##   expr = expr.replace("4!","24").replace(".4'","(4.0/9.0)").replace("r(","sqrt(").replace("^","**")
+##   return eval(expr)
+
+def populate(funcs):
+    for l in range(2,5):
+        populate_level(funcs, l)
+        # print "nums", l, len(nums[l])
+
+# Cuando no se estan generando todas las expresiones, descomentar para saber cuales son estos,
+# los que se guardan en la lista missing
+#
+#    missing = []
+    n4 = nums[4]
+    for n in range(0,101):
+        if n in n4:
+           # Un "backpatch": para no castigar a todos en paren() comparando contra 44, se permite
+           # generar parentesis superfluos alrededor del 44 y aqui se corrige, solo cuando es parte del listado final
+           print n, "=", n4[n].replace("(44)","44")
+## Descomentar para comprobar que las expresiones efectivamente generan el nro adecuado
+##           print "TST", n, n == int(evalme(nums[4][n])+0.000001)
+#        else:
+#           missing.append(n)
+#
+#    if len(missing) > 0:
+#       print "Aun falta generar:", missing
+        
+faraway = -9999
+def midiv(x,y): return float(x)/float(y) if y != 0 else faraway
+      
+def mipow(x,y): return x**y if x >= 0 and x <= 10 and y >= 0 and y <= 5 else faraway
+
+ops = { "+": operator.add, "-": operator.sub, "*": operator.mul, "/": midiv, "^": mipow }
+ 
+if __name__ == '__main__':
+    from time import time
+    t1 = time()
+    populate(ops)
+    t2 = time()
+    print "Tiempo: %g" % (t2-t1)
