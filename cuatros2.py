3a9e0269d4d980faac26c063cae0f0206f15e96f
Rodrigo Chappa <rodrigo.chappa@gmail.com>
2014-02-27 23:14:47 -0300

    Simplificaciones varias al codigo

--- cuatros2.py
+++ cuatros2.py
@@ -1,4 +1,4 @@
-# Solucion usando memoization
+# Solucion usando memoization v.1
 #
 # Construye soluciones usando 1 nro 4, luego 2 nros 4, luego 3 y finalmente 4,
 # al estilo usado por Dynamic Programming
@@ -14,88 +14,74 @@ import operator
  
 from math import sqrt
 
-nums1 = {
-    2: "r(4)", 4: "4", 24: "4!", 4.0/9.0: ".4'", .4: ".4", sqrt(4.0/9.0):"r(.4')"
-}
-nums = {}
+# Construccion bottom up de nums, partiendo por el diccionario de tamano 1
+# (ver populate_level() mas abajo tb)
+nums  = { 1: { 2: "r(4)", 4: "4", 24: "4!", 4.0/9.0: ".4'", .4: ".4", sqrt(4.0/9.0):"r(.4')" },
+          2: { 44: "44" }, 3: {}, 4: {} }
 
+# paren(x): escribe parentesis alrededor de la expresion x, intenta no hacerlo cuando es superfluo
+#
 # reduce el nro de parentesis usados
-def paren(x,force=False):
+# e.g. algunos parentesis superfluos (a*b)+c -> a*b+c
+def paren(x,level,force=False):
     if not force:
-       cnt = x.count("4")
-       if cnt == 1 or x == "44": return x
-       if cnt == 2 and any( op in x for op in ["*","/"] ): return x
+       if level == 1 or x == "44": return x
+       if level == 2 and any( op in x for op in ["*","/"] ): return x
     return "(" + x + ")"
 
-# acerca al entero mas cercano
+# acerca al entero mas cercano, cuando esta cerca de este
+ferror = 0.00001
 def roundme(x):
-    dif = x- int(x+0.5)
-    if dif < 0: dif = -dif 
-    if dif < 0.00001:
-       return int(x+0.5)
+    if abs(x- int(x+ferror)) < ferror: return int(x+ferror)
     return x
 
 # construye tabla de memoization usando los operadores que se pasan
-# en parametro funcs
+# en parametro funcs, la tabla se usa en invocaciones posteriores
 def populate_level(funcs,level):
-   nums[level] = {}
-   if level == 1:
-      nums[1] = {x:nums1[x] for x in nums1}
-      return
 
+   ll = {}  # el largo de cada expresion en este nivel, la idea es usar esto para generar expresiones cortas
+   nl = nums[level]
    for i in range(1,level):
        # generar todas las combinaciones posibles
-       pairs = [[x,y] for x in nums[i] for y in nums[level-i]]
        # usar esas combinaciones para operarlas con todos los operadores
-       newelements = [ (roundme(funcs[op](x[0],x[1])), paren(nums[i][x[0]],op=="^") + op + paren(nums[level-i][x[1]])) for op in funcs for x in pairs ]
+       ni  = nums[i]
+       nli = nums[level-i]
+       newelements = [ (roundme(funcs[op](x,y)), paren(ni[x],i,op=="^") + op + paren(nli[y],level-i)) for op in funcs for x in ni for y in nli ]
        # se tienen varios resultados, tomemos lo mejor
        for k,v in newelements:
-           # se intenta generar la expresion mas corta
-           if k not in nums[level]:
-              nums[level][k] = v
-           else:
-              lnew = len(v)
-              lold = len(nums[level][k])
-              # y tambien se favorece a quienes usan mas "+"
-              if lnew < lold or (lnew == lold and v.count("+") > nums[level][k].count("+")):
-                 nums[level][k] = v
-   # Agrego caso especial de 2 digitos
-   # Puede hacerse tambien usando operador de pegado (no implementado aqui)
-   if level == 2:
-       nums[2][44] = "44"
+           # se usa una expresion mas corta cada vez que se puede
+           lv = len(v)
+           if k not in nl or lv < ll[k]:
+              nl[k] = v
+              ll[k] = lv
 
 # Para comprobar que los datos generados son correctos
-#def evalme(expr):
-#   expr = expr.replace("4!","24").replace(".4'","(4.0/9.0)").replace("r(","sqrt(").replace("^","**")
-#   return eval(expr)
+def evalme(expr):
+   expr = expr.replace("4!","24").replace(".4'","(4.0/9.0)").replace("r(","sqrt(").replace("^","**")
+   return eval(expr)
 
 def populate(funcs):
-    for l in range(1,4+1):
+    for l in range(2,5):
         populate_level(funcs, l)
         # print "nums", l, len(nums[l])
 
     missing = []
-    for n in range(0,100+1):
+    for n in range(0,101):
         if n in nums[4]:
            print n, "=", nums[4][n]
-#           print "TST", n, n == int(evalme(nums[4][n])+0.000001)
+           print "TST", n, n == int(evalme(nums[4][n])+0.000001)
         else:
            missing.append(n)
 
     if len(missing) > 0:
        print "Aun falta generar:", missing
         
-def midiv(x,y):
-    if y == 0: return -9999
-    return float(x)/float(y)
+faraway = -9999
+def midiv(x,y): return float(x)/float(y) if y != 0 else faraway
       
-def mipow(x,y):
-    if x >= 0 and y >= 0 and x <= 10 and y <= 10: return x**y
-    return -9999
+def mipow(x,y): return x**y if x >= 0 and x <= 10 and y >= 0 and y <= 5 else faraway
 
-ops = {
-    "+": operator.add, "-": operator.sub, "*": operator.mul, "/": midiv, "^": mipow,
-}
+ops = { "+": operator.add, "-": operator.sub, "*": operator.mul, "/": midiv, "^": mipow }
  
 if __name__ == '__main__':
     from time import time
